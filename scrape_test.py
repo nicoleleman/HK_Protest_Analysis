40a7795c1638144f9384977ea5f14688d50e675a
Nico Man <nicoman329@outlook.com>
2019-10-01 23:49:05 +0100

    Created a for loop to pull headline, summary and youtube videa link for all articles Added a try except block to handle no youtube link

--- scrape_test.py
+++ scrape_test.py
@@ -5,22 +5,22 @@ source = requests.get('http://coreyms.com').text
 
 soup = BeautifulSoup(source, 'lxml')
 
-article = soup.find('article')
-#print(article.prettify())
+for article in soup.find_all('article'):
+    headline = article.h2.a.text
+    print(f'This is the headline: {headline}')
 
-headline = article.h2.a.text
-print(headline)
+    summary = article.find('div', class_='entry-content').p.text
+    print(f'This is the summary: {summary}')
 
-summary = article.find('div', class_='entry-content').p.text
-print(summary)
+    try:
+        video_source = article.find('iframe', class_='youtube-player')['src']
 
-video_source = article.find('iframe', class_='youtube-player')['src']
-#print(video_source)
+        video_id = video_source.split('/')[4]
+        video_id = video_id.split('?')[0]
 
-#Grabs the video id
-video_id = video_source.split('/')[4]
-video_id = video_id.split('?')[0]
-#print(video_id)
+        yt_link = f'https://youtube.com/watch?v={video_id}'
+    except Exception as e:
+        yt_link = None
 
-yt_link = f'https://youtube.com/watch?v={video_id}'
-print(yt_link)
+    print(yt_link)
+    print()
