becfb9baa042542f8dfd06fe2b1ab43f92f49551
Nico Man <nicoman329@outlook.com>
2019-10-01 23:21:43 +0100

    Writing a for loop to pull in the article title and summary

--- scrape.py
+++ scrape.py
@@ -4,4 +4,17 @@ import requests
 with open('simple.html', 'r') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
-print(soup.prettify())
+match = soup.title.text
+#print(match)
+match2 = soup.div
+#print(match2)
+match3 = soup.find('div', class_='footer')
+#print(match3)
+
+# Get all article headline and summary
+for article in soup.find_all('div', class_='article'):
+    headline = article.h2.a.text
+    print(headline)
+    summary = article.p.text
+    print(summary)
+    print()
