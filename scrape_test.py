9b4c87f9276989bebb49a209db02c0d9faa7ebab
Nico Man <nicoman329@outlook.com>
2019-10-01 23:57:25 +0100

    Writes the data to a CSV file

--- scrape_test.py
+++ scrape_test.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 source = requests.get('http://coreyms.com').text
 
 soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('cms_scrape.csv', 'w')
+
+csv_writer = csv.writer((csv_file))
+csv_writer.writerow(['Headline','Summary','Video Link'])
 
 for article in soup.find_all('article'):
     headline = article.h2.a.text
@@ -24,3 +30,7 @@ for article in soup.find_all('article'):
 
     print(yt_link)
     print()
+
+    csv_writer.writerow([headline,summary,yt_link])
+
+csv_file.close()
