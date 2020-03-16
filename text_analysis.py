29450d69d2f8cc0f056f8f31981df0e0e883a6a2
Nico Man <nicoman329@outlook.com>
2020-03-16 23:58:58 +0000

    Updated the notebook.

--- text_analysis.py
+++ text_analysis.py
@@ -3,9 +3,14 @@ import numpy
 import matplotlib.pyplot as plt
 import seaborn as sns
 import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import LancasterStemmer
 from collections import Counter
 
+
 def count_frequent_words():
+    global Counter
     df = pd.read_csv('csv_files/scmp_article_content_all.csv')
     text_dataset = df['paragraphs']
     #print(type(text_dataset.head()))
@@ -17,5 +22,14 @@ def count_frequent_words():
     Counter = Counter(x for xs in split_it for x in xs)
     most_occur = Counter.most_common(10)
     print(most_occur)
+    return most_occur
 
-nltk.download()
+count_frequent_words()
+
+# plt.figure(figsize=(16,6))
+# pd_most_occur = pd.DataFrame(most_occur, columns = ['Words' , 'Count'])
+# ax = sns.barplot(x='Count', y='Words' , data=pd_most_occur, palette='rocket')
+# ax.set_ylabel('Count', fontsize=15)
+# ax.set_xlabel('Words', fontsize=15)
+# ax.set_title('Top 10 most common words', fontsize=20)
+
